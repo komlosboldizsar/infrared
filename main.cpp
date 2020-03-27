358b9538f47b870d68e6bd0e2fc92eac3e828918
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2020-03-27 18:09:06 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Boldi on 2020. 03. 27..
+//
+
+#include <cstdio>
+#include <iostream>
+
+int main(int argc, char** argv) {
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
